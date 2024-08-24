dd8fc8f5b04a4f4791aa5639c1a1ed74509992f2
Joelft94 <joel_ferrer_94@hotmail.com>
2024-08-24 18:02:46 -0400

    add : invalid location error message

--- src/geocode.js
+++ src/geocode.js
@@ -1,18 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const geocode = async (city, country, apiKey) => {
   try {
-    const geoResponse = await axios.get('http://api.openweathermap.org/geo/1.0/direct', {
-      params: {
-        q: `${city},${country}`,
-        limit: 1,
-        appid: apiKey,
-      },
-    });
+    const geoResponse = await axios.get(
+      "http://api.openweathermap.org/geo/1.0/direct",
+      {
+        params: {
+          q: `${city},${country}`,
+          limit: 1,
+          appid: apiKey,
+        },
+      }
+    );
+
+    if (geoResponse.data.length === 0) {
+      throw new Error(
+        `Invalid location: "${city}, ${country}". Please check the city and country names.`
+      );
+    }
 
     const { lat, lon } = geoResponse.data[0];
     return { lat, lon };
   } catch (error) {
-    throw new Error('Error fetching geolocation: ' + error.message);
+    throw new Error("Error fetching geolocation: " + error.message);
   }
 };
